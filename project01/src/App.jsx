87e2389419b55105acfe6c91dcf1470ba0982f16
Rohit Gunwal <rgunwal345@gmail.com>
2024-08-06 19:18:54 +0530

    Adding another Function Chai in Vite Project

--- project01/src/App.jsx
+++ project01/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import Chai from "/Chai.jsx"
 function App() {
   return (
+    <>
+    
+    <Chai/>
     <div>App</div>
+    </>
   )
 }
-
+// App() mai aap single element hi return kr skte hai is problem se bachne k liye ham saare elements ko ek div mai daal dete hai <> </> complete div na likh k aisa likhenge toh bhi chalega 
 export default App
---
+++ project01/src/Chai.jsx
@@ -0,0 +1,10 @@
+//ye ek convention hai yaha pr ki aap jab components bana rhe hai toh un  components ka naam capital rakhe example
+
+function Chai(){
+    return(
+        <h1>Chai & React </h1>
+    )
+}
+
+export default Chai
+// function ka naam hamesha capital se shure krna padega ye compulsory hai saath mai agar koi component bana rhe hai toh usko .jsx file mai hi daalna padega usko .js mai nhi daal skte agar vite ka use krke project banaya hai toh, create react app mai thodi liberty hoti hai vahan pr .js mai bhi daal skte lekin naam capital vahan bhi rkhna padega
